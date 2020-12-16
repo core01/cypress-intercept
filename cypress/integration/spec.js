65301a32f013c04bbc9c7fb53d975dcf83a1b5c2
Roman Sadoyan <sadoyan@protonmail.com>
2020-12-16 21:53:17 +0300

    add test example

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,6 +1,21 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
-  })
+describe('Cy.intercept', () => {
+
+  beforeEach(() => {
+    cy.intercept('GET', '**/comments/*', (req) => {
+      console.log('here is req', req);
+    })
+  });
+
+  it('Should work', () => {
+    cy.intercept('GET', '**/comments/*').as('getComment')
+
+    cy.visit('https://example.cypress.io/commands/waiting');
+
+
+    cy.get('.network-btn').click()
+
+    cy.wait('@getComment').its('response.statusCode').should('be.oneOf', [200, 304])
+    cy.wait('@getComment').its('response.body.postId').as('postId');
+  });
 })
